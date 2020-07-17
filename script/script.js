0195848d8408fd9576fa12a1d6059136d700349c
A <spaceinbackyard@gmail.com>
2020-07-17 22:23:34 +0500

    Fifth commit

--- script/script.js
+++ script/script.js
@@ -1,5 +1,32 @@
 'use strict';
 
+
+let startBtn = document.getElementById("start"),
+	budgetValue = document.getElementsByClassName('budget-value')[0],
+	dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+	levelValue = document.getElementsByClassName('level-value')[0],
+	expensesValue = document.getElementsByClassName('expenses-value')[0],
+	optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+	incomeValue = document.getElementsByClassName('income-value')[0],
+   monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+   yearSavingsValue = document.getElementsByClassName('yearsavings-value')[0],
+
+
+	expensesItem = document.getElementsByClassName('expenses-item'),
+	expensesBtn = document.getElementsByTagName('button')[0],
+	optionalExpensesBtn = document.getElementsByTagName('button')[1],
+   countBtn = document.getElementsByTagName('button')[2],
+   optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+	incomeItem = document.querySelector('.choose-income'),
+	checkSavings = document.querySelector('#savings'),
+	sumValue = document.querySelector('.choose-sum'),
+   percentValue = document.querySelector('.choose-percent'),
+   yearValue = document.querySelector('.year-value'),
+   monthValue = document.querySelector('.month-value'),
+   dayValue = document.querySelector('.day-value');
+
+
+
 let money, time;
 
 function start() {
@@ -19,7 +46,7 @@ let appData = {
    timeData: time,
    expenses: {},
    optionalExpenses: {},
-   income: [1, 2, 3],
+   income: [],
    savings: true,
    chooseExpenses: function() {
          for (let i = 0; i < 2; i++) {
